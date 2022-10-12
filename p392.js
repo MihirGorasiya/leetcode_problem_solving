950e468515d6a5d746fbb3ead87eb10c2a99ccf3
Mihir Gorasiya <mihirpatel9408@gmail.com>
2022-10-13 00:38:09 +0530

    392 solved

---
+++ p392.js
@@ -0,0 +1,15 @@
+var s = 'axc'
+var t = 'ahbgdc'
+
+var x = 0
+
+for (let i = 0; i < t.length; i++)
+    if (t[i] == s[x])
+        x++
+
+
+
+if (x == s.length)
+    console.log(true)
+else
+    console.log(false)
